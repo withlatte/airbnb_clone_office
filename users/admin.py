f72507e2e8be12010e95682e28ef046e907c9b4c
jpark_office <withlatte@naver.com>
2020-03-17 16:51:35 +0900

    Lesson #3.6 done

---
+++ users/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
+
+
+# TODO below codes start with @ is called decorator
+# it is alternative to below,
+# admin.site.register(models.User,CustomUserAdmin)
+
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+    """ Custom User Admin """
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birth_date",
+                    "language",
+                    "currency",
+                    "super_host",
+                )
+            },
+        ),
+    )
