9108db5fc8717e7696592e69c90361a448a81cca
jpark_office <withlatte@naver.com>
2020-03-27 12:48:12 +0900

    Lesson #9.5 done

--- rooms/management/commands/seed_rooms.py
+++ rooms/management/commands/seed_rooms.py
@@ -22,6 +22,9 @@ class Command(BaseCommand):
         seeder = Seed.seeder()
 
         all_user = user_models.User.objects.all()
+        amenities = room_models.Amenity.objects.all()
+        facilities = room_models.Facility.objects.all()
+        rules = room_models.HouseRule.objects.all()
         room_types = room_models.RoomType.objects.all()
 
         seeder.add_entity(
@@ -39,16 +42,33 @@ class Command(BaseCommand):
                 "baths": lambda x: random.randint(1, 10),
             },
         )
-        created_photos = seeder.execute()
-        created_clean = flatten(list(created_photos.values()))
+        created_photos = (
+            seeder.execute()
+        )  # DB에 저장 후 Dictionary type 으로 {class : primary key}로 리턴
+        created_clean = flatten(list(created_photos.values()))  # 위 Dict pk 값을 List 로 전환
         for pk in created_clean:
             room = room_models.Room.objects.get(pk=pk)
-            for i in range(1, random.randint(3, 14)):
+            for i in range(1, random.randint(3, 30)):
                 room_models.Photo.objects.create(
                     caption=seeder.faker.sentence(),
                     room=room,
                     file=f"room_photos/{random.randint(1, 31)}.webp",
                 )
+            for a in amenities:
+                magic_number = random.randint(0, 15)
+                if magic_number % 2 == 0:
+                    room.amenities.add(a)
+
+            for f in facilities:
+                magic_number = random.randint(0, 15)
+                if magic_number % 2 == 0:
+                    room.facilities.add(f)
+
+            for r in rules:
+                magic_number = random.randint(0, 15)
+                if magic_number % 2 == 0:
+                    room.house_rules.add(r)
+
         self.stdout.write(
             self.style.NOTICE(f"{how_many} rooms are successfully seeded!")
         )
