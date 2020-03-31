b15f8b424ea770138560093ec6431defbf50ddb7
jpark_office <withlatte@naver.com>
2020-03-31 15:30:19 +0900

    Lesson #11.1 done

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from . import models as room_models
 
-# Create your views here.
+
+def all_rooms(request):
+    all_my_rooms = room_models.Room.objects.all()
+    page_size = 10
+    page_num = request.GET.get("page")
+    page_num = int(page_num)
+    limits = page_size * page_num
+    offset = limits - page_size
+    limited_my_rooms = all_my_rooms[offset:limits]
+
+    return render(
+        request, "rooms/home.html", context={"all_my_rooms": limited_my_rooms}
+    )
