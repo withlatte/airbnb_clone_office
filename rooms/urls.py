ee9117e2d001b108e8da5b4b9f9fea68d36d5df7
jpark_office <withlatte@naver.com>
2020-04-02 15:19:11 +0900

    Lesson #13.1 done

--- rooms/urls.py
+++ rooms/urls.py
@@ -4,5 +4,6 @@ from . import views as room_views
 app_name = "rooms"
 
 urlpatterns = [
-    path("<int:pk>", room_views.room_detail, name="detail"),
+    path("<int:pk>", room_views.RoomDetailView.as_view(), name="detail"),
+    path("search/", room_views.search, name="search"),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.urls import reverse
+
+# from django.http import Http404
 from django.utils import timezone
-from django.views.generic import ListView
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django_countries import countries
 from . import models as room_models
 
 
@@ -20,11 +24,35 @@ class HomeView(ListView):
         return context
 
 
-def room_detail(request, pk):
-    return render(request, "rooms/detail.html")
+class RoomDetailView(DetailView):
+    """ Room Detail View Definition """
+
+    # DetailView names "context_obj_name" to name of model which is "Room".
+    # it turns "Room" to small letters "room" and then use it by default.
+    model = room_models.Room
+
+
+def search(request):
+    city = request.GET.get("city", "Anywhere")
+    city = str.capitalize(city)
+    room_types = room_models.RoomType.objects.all()
+
+    return render(
+        request,
+        "rooms/search.html",
+        {"city": city, "countries": countries, "room_types": room_types},
+    )
 
 
 """
+def room_detail(request, pk):
+    try:
+        room = room_models.Room.objects.get(pk=pk)
+        return render(request, "rooms/room_detail.html", {"room": room})
+    except room_models.Room.DoesNotExist:
+        raise Http404()
+        # return redirect(reverse("core:home"))
+
 def all_rooms(request):
     all_my_rooms = room_models.Room.objects.all()
 
